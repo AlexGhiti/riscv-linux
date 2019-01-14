56657242e57f8b067d550c3d07a1befa799b9172
Alexandre Ghiti <alex@ghiti.fr>
2019-01-14 19:40:25 +0100

    hugetlb: allow to free gigantic pages regardless of the configuration
    On systems without CMA or (MEMORY_ISOLATION && COMPACTION) activated but
    that support gigantic pages, boottime reserved gigantic pages can not be
    freed at all. This patchs simply enables the possibility to hand back
    those pages to memory allocator.
    
    This commit then renames gigantic_page_supported and
    ARCH_HAS_GIGANTIC_PAGE to make them more accurate. Indeed, those values
    being false does not mean that the system cannot use gigantic pages: it
    just means that runtime allocation of gigantic pages is not supported,
    one can still allocate boottime gigantic pages if the architecture supports
    it.
    
    Signed-off-by: Alexandre Ghiti <alex@ghiti.fr>

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -8241,8 +8241,9 @@ done:
 				pfn_max_align_up(end), migratetype);
 	return ret;
 }
+#endif
 
-void free_contig_range(unsigned long pfn, unsigned nr_pages)
+void free_contig_range(unsigned long pfn, unsigned int nr_pages)
 {
 	unsigned int count = 0;
 
@@ -8254,7 +8255,6 @@ void free_contig_range(unsigned long pfn, unsigned nr_pages)
 	}
 	WARN(count != 0, "%d pages are still in use!\n", count);
 }
-#endif
 
 #ifdef CONFIG_MEMORY_HOTPLUG
 /*
