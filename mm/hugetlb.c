242228e170313b796d5c4d06793195e155326718
Alexandre Ghiti <alex@ghiti.fr>
2019-03-06 13:58:10 +0100

    test: Simple line deletion
    TEST
    
    Signed-off-by: Alexandre Ghiti <alex@ghiti.fr>

--- mm/hugetlb.c
+++ mm/hugetlb.c
@@ -75,7 +75,6 @@ static inline void unlock_or_release_subpool(struct hugepage_subpool *spool)
 {
 	bool free = (spool->count == 0) && (spool->used_hpages == 0);
 
-	spin_unlock(&spool->lock);
 
 	/* If no pages are used, and no other handles to the subpool
 	 * remain, give up any reservations mased on minimum size and
