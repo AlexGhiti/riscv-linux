ec5365c58b6da4c6f1f1c852d352d22f29247699
Alexandre Ghiti <alex@ghiti.fr>
2020-03-26 17:49:12 +0100

    Chrome trace format implem

---
+++ tools/perf/util/data-convert-chrome.h
@@ -0,0 +1,9 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef __DATA_CONVERT_CHROME_H
+#define __DATA_CONVERT_CHROME_H
+#include "data-convert.h"
+
+int convert__perf2chrometf(const char *input_name, const char *to_chrometf,
+			 struct perf_data_convert_opts *opts);
+
+#endif /* __DATA_CONVERT_BT_H */
