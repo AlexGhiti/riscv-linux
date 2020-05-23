d07f87259367ea046c29fba6686b827a66b6fc59
Alexandre Ghiti <alex@ghiti.fr>
2020-05-23 08:31:34 -0400

    riscv: Align epc register output in show_regs
    epc output value was not aligned with the other register's values: just
    add a space to align them.
    
    Signed-off-by: Alexandre Ghiti <alex@ghiti.fr>

--- arch/riscv/kernel/process.c
+++ arch/riscv/kernel/process.c
@@ -37,7 +37,7 @@ void show_regs(struct pt_regs *regs)
 {
 	show_regs_print_info(KERN_DEFAULT);
 
-	pr_cont("epc: " REG_FMT " ra : " REG_FMT " sp : " REG_FMT "\n",
+	pr_cont("epc : " REG_FMT " ra : " REG_FMT " sp : " REG_FMT "\n",
 		regs->epc, regs->ra, regs->sp);
 	pr_cont(" gp : " REG_FMT " tp : " REG_FMT " t0 : " REG_FMT "\n",
 		regs->gp, regs->tp, regs->t0);
