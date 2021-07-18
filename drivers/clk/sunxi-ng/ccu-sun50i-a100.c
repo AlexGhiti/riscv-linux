26646b3fb1c596af3df70e30cfdc7635fe132ed4
Samuel Holland <samuel@sholland.org>
2021-07-18 17:48:02 -0500

    clk: sunxi-ng: Allow drivers to be built as modules
    While it is useful to build all of the CCU drivers at once, only 1-3 of
    them will be loaded at a time, or possibly none of them if the kernel is
    booted on a non-sunxi platform. These CCU drivers are relatively large;
    32-bit drivers have 30-50k of data each, while the 64-bit ones are
    50-75k due to the increased pointer overhead. About half of that data
    comes from relocations. Let's allow the user to build these drivers as
    modules so only the necessary data is loaded.
    
    As a first step, convert the CCUs that are already platform drivers.
    
    When the drivers are built as modules, normally the file name becomes
    the module name. However, the current file names are inconsistent with
    the <platform>-<peripheral> name used everywhere else: the devicetree
    bindings, the platform driver names, and the Kconfig symbols. Use
    Makfile logic to rename the modules so they follow the usual pattern.
    
    Signed-off-by: Samuel Holland <samuel@sholland.org>

--- drivers/clk/sunxi-ng/ccu-sun50i-a100.c
+++ drivers/clk/sunxi-ng/ccu-sun50i-a100.c
@@ -6,7 +6,6 @@
 #include <linux/clk-provider.h>
 #include <linux/io.h>
 #include <linux/module.h>
-#include <linux/of_address.h>
 #include <linux/platform_device.h>
 
 #include "ccu_common.h"
@@ -1274,3 +1273,6 @@ static struct platform_driver sun50i_a100_ccu_driver = {
 	},
 };
 module_platform_driver(sun50i_a100_ccu_driver);
+
+MODULE_IMPORT_NS(SUNXI_CCU);
+MODULE_LICENSE("GPL");
