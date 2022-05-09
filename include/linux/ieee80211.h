626dec32eabf96e3b31295aba79a89eff205a3c1
Alexandre Ghiti <alexandre.ghiti@canonical.com>
2022-05-09 15:45:08 +0200

    Declare compare_ether_addr instead of changing all the call-sites

--- include/linux/ieee80211.h
+++ include/linux/ieee80211.h
@@ -324,6 +324,22 @@ struct ieee80211_qos_hdr {
 } __packed __aligned(2);
 
 /**
+ * compare_ether_addr - Compare two Ethernet addresses
+ * @addr1: Pointer to a six-byte array containing the Ethernet address
+ * @addr2: Pointer other six-byte array containing the Ethernet address
+ *
+ * Compare two Ethernet addresses, returns 0 if equal, non-zero otherwise.
+ * Unlike memcmp(), it doesn't return a value suitable for sorting.
+ */
+static inline unsigned compare_ether_addr(const u8 *addr1, const u8 *addr2)
+{
+        const u16 *a = (const u16 *) addr1;
+        const u16 *b = (const u16 *) addr2;
+
+        return ((a[0] ^ b[0]) | (a[1] ^ b[1]) | (a[2] ^ b[2])) != 0;
+}
+
+/**
  * ieee80211_has_tods - check if IEEE80211_FCTL_TODS is set
  * @fc: frame control bytes in little-endian byteorder
  */
